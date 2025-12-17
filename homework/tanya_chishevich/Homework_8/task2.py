1e14155162acd115f40fe803864f3bc0015b6b0d
TanyaGit-enter <tanya.sadovskay@gmail.com>
2025-12-17 16:19:59 +0300

    Changed output fibonacci numbers

--- homework/tanya_chishevich/Homework_8/task2.py
+++ homework/tanya_chishevich/Homework_8/task2.py
@@ -1,21 +1,25 @@
-def fib(n):
+import sys
+
+sys.set_int_max_str_digits(100001)
+
+
+def fib():
     a, b = 0, 1
-    count = 0
-    while count < n:
-        yield a
+    count = 1
+    while True:
+        if count == 5:
+            yield a
+        elif count == 200:
+            yield a
+        elif count == 1000:
+            yield a
+        elif count == 100000:
+            yield a
         a, b = b, a + b
         count += 1
 
 
-fib_gen = fib(70)
+fib_gen = fib()
 
 for num in fib_gen:
-    if num == 5:
-        print(num)
-    elif 200 <= num < 300:
-        print(num)
-    elif 1000 <= num < 2000:
-        print(num)
-    elif 100000 <= num < 200000:
-        print(num)
-        break
+    print(num)
