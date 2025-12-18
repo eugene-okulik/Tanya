26aad0496473c8a6f0b30fdcd8f665e24a12ea6b
TanyaGit-enter <tanya.sadovskay@gmail.com>
2025-12-18 15:05:52 +0300

    Changed output fibonacci numbers part 2

--- homework/tanya_chishevich/Homework_8/task2.py
+++ homework/tanya_chishevich/Homework_8/task2.py
@@ -5,21 +5,22 @@ sys.set_int_max_str_digits(100001)
 
 def fib():
     a, b = 0, 1
-    count = 1
     while True:
-        if count == 5:
-            yield a
-        elif count == 200:
-            yield a
-        elif count == 1000:
-            yield a
-        elif count == 100000:
-            yield a
+        yield a
         a, b = b, a + b
-        count += 1
 
 
 fib_gen = fib()
+positions = [5, 200, 1000, 100000]
+results = {}
 
-for num in fib_gen:
-    print(num)
+for pos in positions:
+    num = 0
+    for i in range(pos):
+        num = next(fib_gen)
+    results[pos] = num
+
+print(f"Пятое: {results[5]}")
+print(f"Двухсотое: {results[200]}")
+print(f"Тысячное: {results[1000]}")
+print(f"Стотысячное: {results[100000]}")
