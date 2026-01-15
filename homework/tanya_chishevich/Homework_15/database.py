0920353cc678a9417e74400469f35d84276165cc
TanyaGit-enter <tanya.sadovskay@gmail.com>
2026-01-15 09:00:49 +0300

    Added connect to BD and created sql queries

--- homework/tanya_chishevich/Homework_15/database.py
+++ homework/tanya_chishevich/Homework_15/database.py
@@ -14,10 +14,32 @@ mydb = mysql.connect(
     database="st-onl"
 )
 
+# Создание таблиц
 cursor = mydb.cursor()
+
+query = 'INSERT INTO students (name, second_name) VALUES (%s, %s)'
+values = ('Tanya2', 'Chishevich2')
+cursor.execute(query, values)
+
+new_user_id = cursor.lastrowid # Вот он, ваш ID!
+# mydb.commit()
+print(new_user_id)
+
+books_data = [
+    ('How to Break Web Software 2', new_user_id),
+    ('Software Testing Techniques 2', new_user_id),
+    ('Agile Testing: A Practical Guide for Testers 2', new_user_id)
+]
+query = 'INSERT INTO books (title, taken_by_student_id) VALUES (%s, %s)'
+cursor.executemany(query, books_data)
+# mydb.commit()
+
 query = 'INSERT INTO `groups` (title, start_date, end_date)  VALUES (%s, %s, %s)'
 cursor.execute(query, ('QA tester 2', today_new, future_date))
+group_id = cursor.lastrowid # Вот он, ваш ID!
 mydb.commit()
+
+print(group_id)
 
 
 
