7629d24a4304d707e45b4b6e8172901cac74b0b1
TanyaGit-enter <tanya.sadovskay@gmail.com>
2026-01-17 00:33:36 +0300

    Fixed commits

--- homework/tanya_chishevich/Homework_15/database.py
+++ homework/tanya_chishevich/Homework_15/database.py
@@ -18,23 +18,23 @@ cursor = mydb.cursor()
 
 # Создание студента
 query = 'INSERT INTO students (name, second_name) VALUES (%s, %s)'
-values = ('Tanya2', 'Chishevich2')
+values = ('Tanya3', 'Chishevich3')
 cursor.execute(query, values)
 new_user_id = cursor.lastrowid
 print(f'ID студента: {new_user_id}')
 
 # Создание нескольких книг
 books_data = [
-    ('How to Break Web Software 2', new_user_id),
-    ('Software Testing Techniques 2', new_user_id),
-    ('Agile Testing: A Practical Guide for Testers 2', new_user_id)
+    ('How to Break Web Software 3', new_user_id),
+    ('Software Testing Techniques 3', new_user_id),
+    ('Agile Testing: A Practical Guide for Testers 3', new_user_id)
 ]
 query = 'INSERT INTO books (title, taken_by_student_id) VALUES (%s, %s)'
 cursor.executemany(query, books_data)
 
 # Создание группы
 query = 'INSERT INTO `groups` (title, start_date, end_date)  VALUES (%s, %s, %s)'
-cursor.execute(query, ('QA tester 2', today_new, future_date))
+cursor.execute(query, ('QA tester 3', today_new, future_date))
 group_id = cursor.lastrowid
 print(f'ID группы: {group_id}')
 
@@ -44,55 +44,44 @@ cursor.execute(query, (group_id, new_user_id))
 
 # Создание несколько учебных предметов (subjects)
 subjects_data = [
-    ('Agile testing 2',),
-    ('QA testing 2',),
-    ('Software Test automation 2',)
+    'Agile testing 3',
+    'QA testing 3',
+    'Software Test automation 3'
 ]
 
-query = 'INSERT INTO subjects (title) VALUES (%s)'
-cursor.executemany(query, subjects_data)
+subjects_ids = []
+for subject_name in subjects_data:
+    cursor.execute('INSERT INTO subjects (title) VALUES (%s)', (subject_name,))
+    subject_id = cursor.lastrowid
+    subjects_ids.append(subject_id)
 mydb.commit()
-
-# Получение учебных предметов
-subjects = ['Agile testing 2', 'QA testing 2', 'Software Test automation 2']
-placeholders = ', '.join(['%s'] * len(subjects))
-query = f'SELECT * from subjects where title IN ({placeholders})'
-cursor.execute(query, tuple(subjects))
-
-results = cursor.fetchall()
-print(f'Получение предметов: {results}')
+print(f'Получение всех id предметов: {subjects_ids}')
 
 # Создание по два занятия для каждого предмета
 lessons_data = [
-    ('SCRUM 2', results[0][0]),
-    ('Kanban 2', results[0][0]),
-    ('Functional testing 2', results[1][0]),
-    ('UI/UX 2', results[1][0]),
-    ('Auto testing Python 2', results[2][0]),
-    ('CI/CD 2', results[2][0])
+    ('SCRUM 3', subjects_ids[0]),
+    ('Kanban 3', subjects_ids[0]),
+    ('Functional testing 3', subjects_ids[1]),
+    ('UI/UX 3', subjects_ids[1]),
+    ('Auto testing Python 3', subjects_ids[2]),
+    ('CI/CD 3', subjects_ids[2])
 ]
-
-query = 'INSERT INTO lessons (title, subject_id) VALUES (%s, %s)'
-cursor.executemany(query, lessons_data)
+lessons_ids = []
+for lesson_name, subj_id in lessons_data:
+    cursor.execute('INSERT INTO lessons (title, subject_id) VALUES (%s, %s)', (lesson_name, subj_id))
+    lesson_id = cursor.lastrowid
+    lessons_ids.append(lesson_id)
 mydb.commit()
-
-# Получение занятий
-lessons = ['SCRUM 2', 'Kanban 2', 'Functional testing 2', 'UI/UX 2', 'Auto testing Python 2', 'CI/CD 2']
-placeholders = ', '.join(['%s'] * len(lessons))
-query = f'SELECT * from lessons where title IN ({placeholders})'
-cursor.execute(query, tuple(lessons))
-
-results_lesson = cursor.fetchall()
-print(f'Получение занятий: {results_lesson}')
+print(f'Получение всех id занятий: {lessons_ids}')
 
 # Создание оценок для всех наших занятий
 marks_data = [
-    (5, results_lesson[0][0], new_user_id),
-    (6, results_lesson[1][0], new_user_id),
-    (7, results_lesson[2][0], new_user_id),
-    (8, results_lesson[3][0], new_user_id),
-    (9, results_lesson[4][0], new_user_id),
-    (10, results_lesson[5][0], new_user_id)
+    (5, lessons_ids[0], new_user_id),
+    (6, lessons_ids[1], new_user_id),
+    (7, lessons_ids[2], new_user_id),
+    (8, lessons_ids[3], new_user_id),
+    (9, lessons_ids[4], new_user_id),
+    (10, lessons_ids[5], new_user_id)
 ]
 
 query = 'INSERT INTO marks (value, lesson_id, student_id) VALUES (%s, %s, %s)'
@@ -100,11 +89,13 @@ cursor.executemany(query, marks_data)
 mydb.commit()
 
 # Получение оценок
-cursor.execute(f'SELECT * from marks where student_id = {new_user_id}')
+query = 'SELECT * from marks where student_id = %s'
+cursor.execute(query, (new_user_id,))
 print(f'Получение оценок: {cursor.fetchall()}')
 
 # Получение книг для студента
-cursor.execute(f'SELECT * from books where taken_by_student_id = {new_user_id}')
+query = 'SELECT * from books where taken_by_student_id = %s'
+cursor.execute(query, (new_user_id,))
 print(f'Получение книг нашего студента: {cursor.fetchall()}')
 
 # Вся информация о студенте
@@ -122,11 +113,11 @@ query = f"""
  JOIN subjects s2
  JOIN lessons l ON l.subject_id = s2.id
  JOIN marks m ON m.student_id = s.id and m.lesson_id = l.id
- WHERE s.id = {new_user_id}
+ WHERE s.id = %s
  ORDER BY m.value DESC
 """
 
-cursor.execute(query)
+cursor.execute(query, (new_user_id,))
 print(f'Вся информация о студенте: {cursor.fetchall()}')
 
 mydb.close()
